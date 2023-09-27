400519ed2d103977d9b8acf25fc71e503e146973
mahedEG-dev <123960715+mahedEG-dev@users.noreply.github.com>
2023-09-27 11:51:19 +0100

    updated task 1 of milestone 4

--- milestone_4.py
+++ milestone_4.py
@@ -1,3 +1,4 @@
+# %% Task 1 : Setup the Hangman class and the init functions
 import random
 
 
@@ -17,3 +18,15 @@ class Hangman:
         self.word_list = word_list
         self.list_of_guesses = list_of_guesses
         self.num_lives = num_lives
+
+        # set the word_guessed list as a list of '_' with same size as word string
+        self.word_guessed = ['_' for i in range(len(self.word))]
+
+        # store all the unique characters in the secret word into a list of unique characters
+        unique_chars = list(set([word_ for word_ in word]))
+        # pick from unique characters that are not in guessed list
+        self.num_letters = len(
+            [unique_char for unique_char in unique_chars if unique_char not in self.word_guessed])
+
+        # A list of the guesses that have already been tried. Set this to an empty list initially
+        self.list_of_guesses = []
