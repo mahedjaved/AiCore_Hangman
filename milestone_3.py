994203423dc3c80bf29677afbbdf2013cd61b5b8
mahedEG-dev <123960715+mahedEG-dev@users.noreply.github.com>
2023-09-27 09:37:47 +0100

    added task 3 for milestone 3

--- milestone_3.py
+++ milestone_3.py
@@ -22,3 +22,36 @@ if guess.isalpha() in secret_word:
     
 else:
     print(f"Sorry, {guess} is not in the word. Try again.")
+
+
+# %% Task 3: Check whether the guess is in the word
+!pip3 install random-word pyyaml
+from random_word import RandomWords
+secret_word = RandomWords().get_random_word().replace('-',' ')
+
+def check_guess(guess):
+    """
+    This function tests if guess entered is valid, considering the following
+    cases: a) if it is alphabetical  b) if it is a single character  c) if it
+    lies within the secret word chosen by the computer
+    """
+    guess = guess.lower()
+    if guess.isalpha() and guess.lower() in secret_choice and len(guess) == 1:
+        print(f"Good guess! {guess} is in the word.")
+        return True
+    
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+        return False
+    
+def ask_for_input():
+    """
+    Ask the user for the input
+    """
+    return input("Enter a single letter: ")
+
+while (True):
+    guess = ask_for_input()
+    if (check_guess(guess)):
+        break
+
